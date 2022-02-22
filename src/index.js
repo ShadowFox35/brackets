bc8acd0056930502d2bcd3f84f6e4f793c25a071
ShadowFox35 <94312514+ShadowFox35@users.noreply.github.com>
2022-02-22 19:33:04 +0300

    add: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = [];
+    for(let i = 0; i < str.length; i++){
+      let test = bracketsConfig.find(elem => elem.includes(str[i]));
+      if(str[i] == test[1] && (test[0] == arr[arr.length - 1])) arr.pop();
+      else arr.push(str[i]);
+    }
+    return !arr.length;
 }
